7463b1d685fb354ff9c0d05ba98819149440f7c4
Grokir <grokir13@yandex.ru>
2025-09-27 14:07:14 +0300

    UPD: gen 2 byte salt

--- src/server/hash.py
+++ src/server/hash.py
@@ -1,21 +1,19 @@
 from    Crypto.Hash             import  SHAKE256 
 from    Crypto.Hash.SHAKE256    import  SHAKE256_XOF
 
-def gen_salt() -> str:
-  salt:str = None
-  with open("/dev/urandom", 'r') as rf:
-    salt = rf.read(4).encode('utf-8')
+def gen_salt() -> bytes:
+  salt:bytes = None
+  with open("/dev/urandom", 'br') as rf:
+    salt = rf.read(2)
   return salt
 
-def hash_passwd(password:str) -> str:
+def hash_passwd(password:bytes) -> str:
   def set_fill(ch:str) -> str:
     if (len(ch) < 2):
       ch = '0' + ch
     return ch
 
-  shake: SHAKE256_XOF = SHAKE256.new( 
-    bytes(password + gen_salt(), encoding="utf-8") 
-  )
+  shake: SHAKE256_XOF = SHAKE256.new(password)
 
   data: list = [shake.read(1)[0] for _ in range( (256 // 8) )]
   res:str = ""
