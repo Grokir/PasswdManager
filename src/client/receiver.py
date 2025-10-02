86162016ddf864c4fe8c309beea5af29ab24db9c
Grokir <grokir13@yandex.ru>
2025-10-03 00:56:00 +0300

    ADD methods for get users list and logs

--- src/client/receiver.py
+++ src/client/receiver.py
@@ -1,11 +1,13 @@
 import requests
+# from requests import Response
+import json
 from config import HOST, PORT
 
 
 class Receiver:
 
   def POST(self, dataJSON:dict) -> str:
-    resp: Response = requests.post(f"http://{HOST}:{PORT}/", json=dataJSON)
+    resp: requests.Response = requests.post(f"http://{HOST}:{PORT}/", json=dataJSON)
     res:dict = resp.json()
     
     if res["status"] == "error":
@@ -13,13 +15,31 @@ class Receiver:
     
     return res["status"]
     
-  def GET(self, dataJSON:dict):
-    resp: Response = requests.get(f"http://{HOST}:{PORT}/", json=dataJSON)
+  def GET(self, dataJSON:dict) -> tuple[str, dict]:
+    resp: requests.Response = requests.get(f"http://{HOST}:{PORT}/", json=dataJSON)
     res:dict = resp.json()
     
     if res["status"] == "error":
       return res["message"]
     
-    return res["user_data"]
+    return json.loads(res["user_data"])
+  
+  def GET_all_users(self, dataJSON:dict) -> tuple[str, list]:
+    resp: requests.Response = requests.get(f"http://{HOST}:{PORT}/all_users", json=dataJSON)
+    res:dict = resp.json()
+    
+    if res["status"] == "error":
+      return res["message"]
+    
+    return json.loads(res["user_data"])
+  
+  def GET_logs(self, dataJSON:dict) -> tuple[str, list]:
+    resp: requests.Response = requests.get(f"http://{HOST}:{PORT}/logs", json=dataJSON)
+    res:dict = resp.json()
+    
+    if res["status"] == "error":
+      return res["message"]
+    
+    return json.loads(res["logs"])
 
 
