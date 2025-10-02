5cc3363166ac0b69df9c4fe653fda04b1b43a19a
Grokir <grokir13@yandex.ru>
2025-10-03 00:55:31 +0300

    ADD print all users and print logs in admin panel

--- src/client/gui.py
+++ src/client/gui.py
@@ -19,13 +19,15 @@ class GUI_APP:
   __entry_password  = None
   __receiver        = Receiver()
 
-  __current_user: dict = {}
-  __activity:     dict = {
+  __current_user:       dict = {}
+  __activity_frame:     dict = {
     "admin":      False,
     "super_user": False,
     "user" :      False,
     "guest":      False,
-    "login":      False
+    "login":      False,
+    "tree1":      False,
+    "tree2":      False
   }
 
   def __init__(self, window_size:str = "800x600") -> None:
@@ -41,27 +43,30 @@ class GUI_APP:
     self.__init_admin_panel()
     self.__init_user_panel()
 
+
   def run(self):
     # Запуск главного цикла
     self.__window.mainloop()
 
   def __change_frame(self, frame:str):
-    self.__activity["login"] = False
+    self.__activity_frame["login"] = False
     panel: tk.Frame = None
 
     match frame:
       case "admin":
         panel = self.__admin_panel
-        self.__activity["admin"] = True
+        self.__activity_frame["admin"] = True
       case "super_user":
-        panel = self.__suser_panel
-        self.__activity["super_user"] = True
+        # panel = self.__suser_panel
+        panel = self.__user_panel
+        self.__activity_frame["super_user"] = True
       case "user":
         panel = self.__user_panel
-        self.__activity["user"] = True
+        self.__activity_frame["user"] = True
       case "guest":
-        panel = self.__guest_panel
-        self.__activity["guest"] = True
+        # panel = self.__guest_panel
+        panel = self.__user_panel
+        self.__activity_frame["guest"] = True
     
     self.__login_frame.pack_forget()
     panel.pack(fill=tk.BOTH, expand=True)
@@ -77,20 +82,23 @@ class GUI_APP:
           "login": username,
           "password": password
         }
-
+        res_str:str = ""
         try:
 
-          res_str:str = self.__receiver.POST(data)
-          if res_str == "ok":
-            self.__current_user = self.__receiver.GET({"login": username})
-            messagebox.showinfo("Успех", f"Вход выполнен для пользователя: {username}")
-            self.__change_frame(self.__current_user["role"])
-          else:
-            messagebox.showerror("Ошибка", res_str)
+          res_str = self.__receiver.POST(data)
+
         except Exception as e:
           print(e.args)
           exc_str = str(e)
-          messagebox.showerror("Ошибка", "Ошибка подключения к серверу")
+          # messagebox.showerror("Ошибка", "Ошибка подключения к серверу")
+          messagebox.showerror("Ошибка", exc_str)
+        
+        if res_str == "ok":
+          self.__current_user = self.__receiver.GET({"login": username})
+          messagebox.showinfo("Успех", f"Вход выполнен для пользователя: {username}")
+          self.__change_frame(str(self.__current_user["role"]))
+        else:
+          messagebox.showerror("Ошибка", res_str)
       else:
         messagebox.showerror("Ошибка", 
           "Некорректный пароль! Пароль должен быть длиной не менее 8 символов и содержать символы A-Z, a-z, 0-9 и спец. символы"
@@ -98,34 +106,37 @@ class GUI_APP:
 
     else:
       messagebox.showerror("Ошибка", "Пожалуйста, введите логин и пароль")
+    
 
   def __cancel(self):
     self.__window.quit()
 
   def __logout(self):
-    self.__activity["login"] = True
+    self.__activity_frame["login"] = True
     panel: tk.Frame = None
 
     frame:str = ""
 
-    for k in self.__activity.keys():
-      if self.__activity[k]:
+    for k in self.__activity_frame.keys():
+      if self.__activity_frame[k]:
         frame = k
         break
 
     match frame:
       case "admin":
         panel = self.__admin_panel
-        self.__activity["admin"] = False
+        self.__activity_frame["admin"] = False
       case "super_user":
-        panel = self.__suser_panel
-        self.__activity["super_user"] = False
+        # panel = self.__suser_panel
+        panel = self.__user_panel
+        self.__activity_frame["super_user"] = False
       case "user":
         panel = self.__user_panel
-        self.__activity["user"] = False
+        self.__activity_frame["user"] = False
       case "guest":
-        panel = self.__guest_panel
-        self.__activity["guest"] = False
+        # panel = self.__guest_panel
+        panel = self.__user_panel
+        self.__activity_frame["guest"] = False
 
     # Возвращаемся к авторизации: скрываем рабочий фрейм и показываем логин
     panel.pack_forget()
@@ -144,7 +155,7 @@ class GUI_APP:
     """
     returns True if the password characters 
     belong to a set of characters {A-Z, a-z, 
-    special characters, and numbers} and password len = 8. 
+    special characters, and numbers} and password len >= 8. 
     And returns False otherwise.
     """
     regex: str = "^[a-zA-Z" + str(punctuation) + "0-9]"
@@ -152,7 +163,7 @@ class GUI_APP:
     return ( (pattern.search(password) is not None) and (len(password) >= 8) )
   
   def __init_login_panel(self) -> None:
-    self.__activity["login"] = True
+    self.__activity_frame["login"] = True
 
     self.__login_frame = tk.Frame(self.__window)
     self.__login_frame.pack(fill=tk.BOTH, expand=True)
@@ -182,24 +193,87 @@ class GUI_APP:
 
 
   def __init_admin_panel(self) -> None:
-    def load_table_data():
-      self.__receiver
-      for item in self.__tree.get_children():
-            self.__tree.delete(item)
+    def load_table_user_data():
+      if self.__activity_frame["tree2"]:
+        self.__admin_tree2.pack_forget()
+        self.__activity_frame["tree2"] = False
+
+      # отрисуем таблицу и выведем данные
+      self.__admin_tree1.pack(fill=tk.BOTH, expand=True)
+      self.__activity_frame["tree1"] = True
+      
+      for item in self.__admin_tree1.get_children():
+            self.__admin_tree1.delete(item)
       # Пример: получаем данные из Receiver (адаптируйте под ваш API)
       try:
-        login:str = self.__entry_username.get()
-        data_dict = self.__receiver.GET({"login": login})  # Предполагаем, что возвращает список словарей
+        # login:str = self.__entry_username.get()
+        data_dict = self.__receiver.GET_all_users(
+          {
+            "login": self.__current_user["login"], 
+            "role":  self.__current_user["role"]
+          }
+        )  # Предполагаем, что возвращает список словарей
         # Вставляем данные в таблицу
-        self.__tree.insert("", tk.END, values=(
-            data_dict["login"], 
-            data_dict["full_name"], 
-            data_dict["position"],
-            data_dict["role"]
+        for i in range(len(data_dict)):
+          self.__admin_tree1.insert("", tk.END, values=(
+              data_dict[i]["login"], 
+              data_dict[i]["full_name"], 
+              data_dict[i]["position"],
+              data_dict[i]["role"]
+            )
           )
-        )
       except Exception as e:
         messagebox.showerror("Ошибка", f"Не удалось загрузить данные: {e}")
+
+    def load_table_log_data():
+      def parse_log(log_line:str) -> tuple[str, str]:
+        timestamp_end:int = log_line.find(']')
+        timestamp: str = log_line[1:timestamp_end]
+
+        # Остальная часть строки
+        log_message:str = log_line[timestamp_end+2:].strip("'")
+
+        return timestamp, log_message
+
+      # закроем другое дерево, если оно активно
+      if self.__activity_frame["tree1"]:
+        self.__admin_tree1.pack_forget()
+        self.__activity_frame["tree1"] = False
+
+      # отрисуем таблицу и выведем данные
+      self.__admin_tree2.pack(fill=tk.BOTH, expand=True)
+      self.__activity_frame["tree2"] = True
+
+      for item in self.__admin_tree2.get_children():
+            self.__admin_tree2.delete(item)
+      # Пример: получаем данные из Receiver (адаптируйте под ваш API)
+      try:
+        # login:str = self.__entry_username.get()
+        data_list = self.__receiver.GET_logs(
+          {
+            "login": self.__current_user["login"], 
+            "role":  self.__current_user["role"]
+          }
+        )  # Предполагаем, что возвращает список словарей
+        # Вставляем данные в таблицу
+        for i in range(len(data_list)):
+          time, msg = parse_log(data_list[i][:-1])
+          self.__admin_tree2.insert("", tk.END, values=(time, msg))
+          # print(line)
+
+      except Exception as e:
+        messagebox.showerror("Ошибка", f"Не удалось загрузить данные: {e}")
+
+    def close_tables():
+      if self.__activity_frame["tree1"]:
+        self.__admin_tree1.pack_forget()
+        self.__activity_frame["tree1"] = False
+
+      if self.__activity_frame["tree2"]:
+        self.__admin_tree2.pack_forget()
+        self.__activity_frame["tree2"] = False
+
+      # self.__scrollbar.pack_forget()
 
     self.__admin_panel = tk.Frame(self.__window)
 
@@ -207,19 +281,22 @@ class GUI_APP:
 
     header_frame.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
 
-    label_user_info = tk.Label(header_frame, text=f"admin", font=("Arial", 10), fg="blue")
+    label_user_info = tk.Label(header_frame, text=f"admin", font=("Arial", 16), fg="blue")
     label_user_info.pack(side=tk.RIGHT)
 
-    label_welcome = tk.Label(self.__admin_panel, text="", font=("Arial", 14))
+    label_welcome = tk.Label(self.__admin_panel, text="", font=("Arial", 16))
     label_welcome.pack(pady=50)
 
     # Пример элементов рабочей панели (можно добавить больше)
     # button_action1 = tk.Button(self.__admin_panel, text="Действие 1", command=lambda: messagebox.showinfo("Действие", "Выполнено действие 1"))
-    button_action1 = tk.Button(self.__admin_panel, text="Действие 1", command=load_table_data)
+    button_action1 = tk.Button(self.__admin_panel, text="Получить список работников", command=load_table_user_data)
     button_action1.pack(pady=10)
 
-    # button_action2 = tk.Button(self.__admin_panel, text="Действие 2", command=lambda: messagebox.showinfo("Действие", "Выполнено действие 2"))
-    # button_action2.pack(pady=10)
+    button_action2 = tk.Button(self.__admin_panel, text="Показать логи", command=load_table_log_data)
+    button_action2.pack(pady=10)
+
+    button_close_tables = tk.Button(self.__admin_panel, text="Закрыть панель", command=close_tables)
+    button_close_tables.pack(pady=10)
 
     button_logout = tk.Button(self.__admin_panel, text="Выход", command=self.__logout)
     button_logout.pack(pady=20)
@@ -230,25 +307,39 @@ class GUI_APP:
     table_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
     # Создаём Treeview (таблицу) с столбцами
-    self.__tree = ttk.Treeview(table_frame, columns=("login", "ФИО", "Должность", "Роль"), show="headings")
-    self.__tree.pack(fill=tk.BOTH, expand=True)
+    self.__admin_tree1 = ttk.Treeview(table_frame, columns=("login", "ФИО", "Должность", "Роль"), show="headings")
 
     # Определяем заголовки столбцов
-    self.__tree.heading("login", text="login")
-    self.__tree.heading("ФИО", text="ФИО")
-    self.__tree.heading("Должность", text="Должность")
-    self.__tree.heading("Роль", text="Роль")
+    self.__admin_tree1.heading("login", text="login")
+    self.__admin_tree1.heading("ФИО", text="ФИО")
+    self.__admin_tree1.heading("Должность", text="Должность")
+    self.__admin_tree1.heading("Роль", text="Роль")
 
     # Устанавливаем ширину столбцов (опционально)
-    self.__tree.column("login", width=50)
-    self.__tree.column("ФИО", width=150)
-    self.__tree.column("Должность", width=100)
-    self.__tree.column("Роль", width=100)
+    self.__admin_tree1.column("login", width=50)
+    self.__admin_tree1.column("ФИО", width=150)
+    self.__admin_tree1.column("Должность", width=100)
+    self.__admin_tree1.column("Роль", width=100)
 
     # Добавляем scrollbar для прокрутки
-    scrollbar = ttk.Scrollbar(table_frame, orient=tk.VERTICAL, command=self.__tree.yview)
-    self.__tree.configure(yscroll=scrollbar.set)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    self.__scrollbar = ttk.Scrollbar(table_frame, orient=tk.VERTICAL, command=self.__admin_tree1.yview)
+    self.__admin_tree1.configure(yscroll=self.__scrollbar.set)
+
+    
+
+    self.__admin_tree2 = ttk.Treeview(table_frame, columns=("Дата и Время", "Сообщение"), show="headings")
+
+    # Определяем заголовки столбцов
+    self.__admin_tree2.heading("Дата и Время", text="Дата и Время")
+    self.__admin_tree2.heading("Сообщение", text="Сообщение")
+
+    # Устанавливаем ширину столбцов (опционально)
+    self.__admin_tree2.column("Дата и Время", width=100)
+    self.__admin_tree2.column("Сообщение", width=500)
+
+    # Добавляем scrollbar для прокрутки
+    self.__admin_tree2.configure(yscroll=self.__scrollbar.set)
+    self.__scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
     # Кнопка для обновления таблицы
     # button_refresh = tk.Button(self.__admin_panel, text="Обновить таблицу", command=load_table_data)
@@ -266,7 +357,7 @@ class GUI_APP:
 
     header_frame.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
 
-    label_user_info = tk.Label(header_frame, text=f"admin", font=("Arial", 10), fg="blue")
+    label_user_info = tk.Label(header_frame, text="user", font=("Arial", 14), fg="blue")
     label_user_info.pack(side=tk.RIGHT)
 
     label_welcome = tk.Label(self.__user_panel, text="", font=("Arial", 14))
