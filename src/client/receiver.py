affdac89ced95147951bf1093b9f89e2be1847be
Grokir <grokir13@yandex.ru>
2025-10-07 18:42:36 +0300

    ADD метод для обновления пароля

--- src/client/receiver.py
+++ src/client/receiver.py
@@ -6,7 +6,7 @@ from config import HOST, PORT
 
 class Receiver:
 
-  def POST(self, dataJSON:dict) -> str:
+  def login(self, dataJSON:dict) -> str:
     resp: requests.Response = requests.post(f"http://{HOST}:{PORT}/", json=dataJSON)
     res:dict = resp.json()
     
@@ -15,7 +15,7 @@ class Receiver:
     
     return res["status"]
     
-  def GET(self, dataJSON:dict) -> tuple[str, dict]:
+  def GET_current_user(self, dataJSON:dict) -> tuple[str, dict]:
     resp: requests.Response = requests.get(f"http://{HOST}:{PORT}/", json=dataJSON)
     res:dict = resp.json()
     
@@ -41,5 +41,14 @@ class Receiver:
       return res["message"]
     
     return json.loads(res["logs"])
+  
+  def change_password(self, dataJSON:dict) -> str:
+    resp: requests.Response = requests.put(f"http://{HOST}:{PORT}/", json=dataJSON)
+    res:  dict = resp.json()
+    
+    if res["status"] == "error":
+      return res["message"]
+    
+    return res["status"]
 
 
