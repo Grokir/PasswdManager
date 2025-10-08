2409bdf8a28c2138bd76594987e1df2857c47f9f
Grokir <grokir13@yandex.ru>
2025-10-08 22:34:43 +0300

    ADD методы для удаления и добавления пользователей. Изменён метод обновления пользователя

--- src/client/receiver.py
+++ src/client/receiver.py
@@ -15,7 +15,7 @@ class Receiver:
     
     return res["status"]
     
-  def GET_current_user(self, dataJSON:dict) -> tuple[str, dict]:
+  def get_current_user(self, dataJSON:dict) -> tuple[str, dict]:
     resp: requests.Response = requests.get(f"http://{HOST}:{PORT}/", json=dataJSON)
     res:dict = resp.json()
     
@@ -24,7 +24,7 @@ class Receiver:
     
     return json.loads(res["user_data"])
   
-  def GET_all_users(self, dataJSON:dict) -> tuple[str, list]:
+  def get_all_users(self, dataJSON:dict) -> tuple[str, list]:
     resp: requests.Response = requests.get(f"http://{HOST}:{PORT}/all_users", json=dataJSON)
     res:dict = resp.json()
     
@@ -33,7 +33,7 @@ class Receiver:
     
     return json.loads(res["user_data"])
   
-  def GET_logs(self, dataJSON:dict) -> tuple[str, list]:
+  def get_logs(self, dataJSON:dict) -> tuple[str, list]:
     resp: requests.Response = requests.get(f"http://{HOST}:{PORT}/logs", json=dataJSON)
     res:dict = resp.json()
     
@@ -43,7 +43,25 @@ class Receiver:
     return json.loads(res["logs"])
   
   def change_password(self, dataJSON:dict) -> str:
+    resp: requests.Response = requests.patch(f"http://{HOST}:{PORT}/", json=dataJSON)
+    res:  dict = resp.json()
+    
+    if res["status"] == "error":
+      return res["message"]
+    
+    return res["status"]
+  
+  def add_user(self, dataJSON:dict) -> str:
     resp: requests.Response = requests.put(f"http://{HOST}:{PORT}/", json=dataJSON)
+    res:  dict = resp.json()
+    
+    if res["status"] == "error":
+      return res["message"]
+    
+    return res["status"]
+  
+  def del_user(self, dataJSON:dict) -> str:
+    resp: requests.Response = requests.delete(f"http://{HOST}:{PORT}/", json=dataJSON)
     res:  dict = resp.json()
     
     if res["status"] == "error":
